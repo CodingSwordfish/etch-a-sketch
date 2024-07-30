1a92842a3eaa830f70ae1c1e2ebebeec03556863
CodingSwordfish <wagobryan@gmail.com>
2024-07-30 23:57:42 +0300

    Create the size button and the reset button

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded",(e)=>{
-    createBoard(16)
+    let board = document.querySelector("#board");
+    let reset = document.querySelector("#reset");  
 })
 
 function createBoard(size){
     
-         let board = document.querySelector("#board");
+         
         let squareBoxes = size * size;
         for(let i = 0; i< squareBoxes; i++){
             let styles = window.getComputedStyle(board);
@@ -19,11 +20,39 @@ function createBoard(size){
             board.appendChild(divs);
     
         }
+        let count = document.querySelectorAll("div").length;
+          console.log(count)
+       }
+       createBoard(16)
+       function getSize(){
+        let input = prompt("Enter a number between 1 and 40");
+        let message = document.querySelector("#message");
+        if(input==""){
+            message.textContent = "Please enter a number between 1 and 40";
+            alert("Enter a number between 1 and 40")
+        }else if(input<0 || input>40){
+            message.textContent = "Provide a number between 1 and 40";
+            alert("Enter a number between 1 and 40");
+        }else {
+            message.textContent = "You are ready to draw!"
+            return input;
+        }
        }
  
- 
+       let btn_popup = document.querySelector("#popup");
+       btn_popup.addEventListener("click",(e)=>{
+        board.innerHTML = "";
+         let size = getSize();
+          createBoard(size)
+          
+       })
 
-
+reset.addEventListener("click",(e)=>{
+    board.innerHTML = "";
+    createBoard(16);
+    alert("Board has been Reset, You are ready to Draw!");
+})
+      
 
 
 
