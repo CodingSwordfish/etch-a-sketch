636bd938bc2aef4fb21bd4804bc12bcf96419b70
CodingSwordfish <wagobryan@gmail.com>
2024-07-31 11:13:43 +0300

    Modify the size and reset button to function properly

--- index.js
+++ index.js
@@ -1,58 +1,133 @@
 document.addEventListener("DOMContentLoaded",(e)=>{
     let board = document.querySelector("#board");
     let reset = document.querySelector("#reset");  
+    let white = document.querySelector("#white");
+    let message = document.querySelector("#message");
+
+    
 })
 
-function createBoard(size){
-    
-         
-        let squareBoxes = size * size;
-        for(let i = 0; i< squareBoxes; i++){
-            let styles = window.getComputedStyle(board);
-            let width = parseInt(styles.width);
-            let height = parseInt(styles.height);
-            // let border = styles.border;
-            let divs = document.createElement("div");
-            
-            divs.style.width = `${width/size}px`;
-            divs.style.height = `${height/size}px`;
-            
-            board.appendChild(divs);
-    
-        }
-        let count = document.querySelectorAll("div").length;
-          console.log(count)
-       }
-       createBoard(16)
-       function getSize(){
-        let input = prompt("Enter a number between 1 and 40");
-        let message = document.querySelector("#message");
-        if(input==""){
-            message.textContent = "Please enter a number between 1 and 40";
-            alert("Enter a number between 1 and 40")
-        }else if(input<0 || input>40){
-            message.textContent = "Provide a number between 1 and 40";
-            alert("Enter a number between 1 and 40");
-        }else {
-            message.textContent = "You are ready to draw!"
-            return input;
-        }
-       }
- 
-       let btn_popup = document.querySelector("#popup");
-       btn_popup.addEventListener("click",(e)=>{
-        board.innerHTML = "";
-         let size = getSize();
-          createBoard(size)
-          
-       })
+message.textContent = "Board resolution is 16 x 16. You are ready to draw!";
+message.style.fontWeight= "bold";
+    message.style.color = "green";
 
+
+
+    function createBoard(size){
+        let squareBoxes = size * size;
+            for(let i = 0; i< squareBoxes; i++){
+                let styles = window.getComputedStyle(board);
+                let width = parseInt(styles.width);
+                let height = parseInt(styles.height);
+                // let border = styles.border;
+                let divs = document.createElement("div");
+               
+                
+                divs.style.width = `${width/size}px`;
+                divs.style.height = `${height/size}px`;
+                
+               
+                board.appendChild(divs);
+                
+               
+                
+                
+           }
+          
+           
+           
+           
+          
+        }
+           createBoard(16)
+           
+           
+    
+           function getSize(){
+            let input = prompt("Enter a number between 1 and 45");
+            if(input === null){
+                createBoard(16);
+                
+                    
+                    
+                    
+                
+                // message.textContent = "Provide a number between 1 and 45";
+                
+        
+                alert("Enter a number between 1 and 45");
+                message.textContent = `Board resolution default is 16 x 16.`
+                message.style.color = "green";
+                message.style.fontWeight= "bold";
+                
+                
+                
+            }
+            
+            if(input==""){
+                createBoard(16)
+                // message.textContent = "Please enter a number between 1 and 45";
+                // message.style.fontWeight= "bold";
+        // message.style.color = "orange";
+                alert("Enter a number between 1 and 45")
+                message.textContent = `Board resolution default is 16 x 16.`
+                message.style.color = "green";
+                message.style.fontWeight= "bold";
+                
+            }
+            
+            else if(input<0 || input>45){
+                message.textContent = "Provide a number between 1 and 45";
+                message.style.fontWeight= "bold";
+        message.style.color = "orange";
+                alert("Enter a number between 1 and 45");
+                createBoard(16)
+            }
+            
+            else if(input){
+               
+                
+                message.textContent = "";
+                message.textContent += `Board resolution is ${input} x ${input}. \n
+                You are ready to draw! `;
+                
+                message.style.fontWeight= "bold";
+        message.style.color = "green";
+                return input;
+            }
+           
+        }
+    
+           let btn_popup = document.querySelector("#popup");
+           btn_popup.addEventListener("click",(e)=>{
+           
+            
+            board.innerHTML = "";
+            
+             let size = getSize();
+              createBoard(size)
+              
+           })
+
+      
 reset.addEventListener("click",(e)=>{
     board.innerHTML = "";
     createBoard(16);
-    alert("Board has been Reset, You are ready to Draw!");
+    alert("Board Resetting, Get ready to Draw!");
+    message.textContent = "";
+    message.textContent=`Board resolution is 16 x 16. You are ready to draw!`
+    message.style.fontWeight= "bold";
+    message.style.color = "green";
 })
       
+
+function colorDiv(){
+
+}
+
+function blackPen() {
+
+}
 
 
 
