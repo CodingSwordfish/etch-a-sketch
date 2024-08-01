cf7d8b3b9cf79d1f30d4e763dec318b7edbfd1db
CodingSwordfish <wagobryan@gmail.com>
2024-08-01 18:28:32 +0300

    Modify the toggle, make the buttons work correctly and add an erase button

--- index.js
+++ index.js
@@ -3,18 +3,15 @@ document.addEventListener("DOMContentLoaded",(e)=>{
     let reset = document.querySelector("#reset");  
     let white = document.querySelector("#white");
     let message = document.querySelector("#message");
-let random = document.querySelector("#random");
-
-    
+    let random = document.querySelector("#random");
+    let erase = document.querySelector("#erase");
 })
 
 message.textContent = "Board resolution is 16 x 16. You are ready to draw!";
 message.style.fontWeight= "bold";
-    message.style.color = "green";
+message.style.color = "green";
 
-
-
-    function createBoard(size){
+function createBoard(size){
         let squareBoxes = size * size;
             for(let i = 0; i< squareBoxes; i++){
                 let styles = window.getComputedStyle(board);
@@ -26,92 +23,51 @@ message.style.fontWeight= "bold";
                 divs.classList.add("square");
                 divs.style.width = `${width/size}px`;
                 divs.style.height = `${height/size}px`;
-                
-               
                 board.appendChild(divs);
-                
-               
-                
-                
-           }
-          
+              }
+          }
+          createBoard(16)
            
-           
-           
-          
-        }
-           createBoard(16)
-           
-           
-    
-           function getSize(){
+        function getSize(){
             let input = prompt("Enter a number between 1 and 45");
             if(input === null){
-                createBoard(16);
-                
-                    
-                    
-                    
-                
-                // message.textContent = "Provide a number between 1 and 45";
-                
-        
-                alert("Enter a number between 1 and 45");
-                message.textContent = `Board resolution default is 16 x 16.`
-                message.style.color = "green";
-                message.style.fontWeight= "bold";
-                
-                
-                
+            createBoard(16);
+            alert("Enter a number between 1 and 45");
+            message.textContent = `Board resolution default is 16 x 16.`
+            message.style.color = "green";
+            message.style.fontWeight= "bold";
             }
             
             if(input==""){
-                createBoard(16)
-                // message.textContent = "Please enter a number between 1 and 45";
-                // message.style.fontWeight= "bold";
-        // message.style.color = "orange";
-                alert("Enter a number between 1 and 45")
-                message.textContent = `Board resolution default is 16 x 16.`
-                message.style.color = "green";
-                message.style.fontWeight= "bold";
-                
-            }
-            
-            else if(input<0 || input>45){
-                message.textContent = "Provide a number between 1 and 45";
-                message.style.fontWeight= "bold";
-        message.style.color = "orange";
-                alert("Enter a number between 1 and 45");
-                createBoard(16)
-            }
-            
-            else if(input){
-               
-                
-                message.textContent = "";
-                message.textContent += `Board resolution is ${input} x ${input}. \n
-                You are ready to draw! `;
-                
-                message.style.fontWeight= "bold";
-        message.style.color = "green";
+              createBoard(16)
+              alert("Enter a number between 1 and 45")
+              message.textContent = `Board resolution default is 16 x 16.`
+              message.style.color = "green";
+              message.style.fontWeight= "bold";
+              } else if(input<0 || input>45){
+              message.textContent = "Provide a number between 1 and 45";
+              message.style.fontWeight= "bold";
+              message.style.color = "orange";
+              alert("Enter a number between 1 and 45");
+              createBoard(16)
+            }else if(input){
+               message.textContent = "";
+               message.textContent += `Board resolution is ${input} x ${input}. \n
+               You are ready to draw! `;
+              message.style.fontWeight= "bold";
+              message.style.color = "green";
                 return input;
             }
-           
-        }
+           }
     
            let btn_popup = document.querySelector("#popup");
            btn_popup.addEventListener("click",(e)=>{
-           
-            
-            board.innerHTML = "";
-            
-             let size = getSize();
-              createBoard(size)
-              
-           })
+           board.innerHTML = "";
+           let size = getSize();
+           createBoard(size)
+             })
 
-      
-reset.addEventListener("click",(e)=>{
+  reset.addEventListener("click",(e)=>{
     board.innerHTML = "";
     createBoard(16);
     alert("Board Resetting, Get ready to Draw!");
@@ -120,52 +76,37 @@ reset.addEventListener("click",(e)=>{
     message.style.fontWeight= "bold";
     message.style.color = "green";
 })
-      
-
-
+    
 let isDrawing = false;
 
-white.addEventListener("click", (e) => {
-    let color = "white"; // Store the chosen color
-  
-  document.querySelector("body").addEventListener("click",(e)=>{
-
-    isDrawing = !isDrawing;
-
-    
-    
-  })
-    // Event listener on the board (assuming the div container has an id of "board")
-    document.getElementById("board").addEventListener("mouseover", (e) => {
-        if (isDrawing && e.target.classList.contains("square")) { // Check for class "square"
-          e.target.style.backgroundColor = color;
+  white.addEventListener("click", (e) => {
+          let color = "white"; // Store the chosen color
+         document.getElementById("board").addEventListener("mouseover", (e) => {
+         if (isDrawing && e.target.classList.contains("square")) { 
+        e.target.style.backgroundColor = color;
         }
       });
   });
 
-  
   random.addEventListener("click", (e) => {
-      document.querySelector("body").addEventListener("click",(e)=>{
-
-        
-        isDrawing = !isDrawing;
-        
-      })
-      document.getElementById("board").addEventListener("mouseover", (e) => {
+        document.getElementById("board").addEventListener("mouseover", (e) => {
         if (isDrawing && e.target.classList.contains("square")) { // Check for class "square"
           e.target.style.backgroundColor = `hsl(${Math.floor(Math.random() * 360)}, ${Math.floor(Math.random() * 100) + 25}%, ${Math.floor(Math.random() * 75) + 25}%)`;
         } 
       });
-             
-   
-    
-    
+      });
+
+  erase.addEventListener("click", (e) => {
+        document.getElementById("board").addEventListener("mouseover", (e) => {
+        if (isDrawing && e.target.classList.contains("square")) { // Check for class "square"
+          e.target.style.backgroundColor = `black`;
+        } 
+      });
+      });
+
+  document.body.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'BUTTON') {
+      isDrawing = !isDrawing;
+    }
   });
   
-// function setColor(colorChoice) {
-
-
-// }
-
-
-
